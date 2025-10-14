4f0ef00ec9ff7ece3bb32b717ff048112e979bdb
grey <2437313414@qq.com>
2025-10-14 21:33:43 +0800

    Remove unused event for hello message in realtime service

--- app/models/events.py
+++ app/models/events.py
@@ -23,6 +23,7 @@ class EventType(Enum):
     RECORD_CALL_END = "record.call_end"
 
     PHONE_SERVICE_ADD_DIALOG_ENTRY = "phone_service.add_dialog_entry"
+    REDIS_CREATE_CALL_RECORD = "redis.create_call_record"
     REDIS_BIND_DIALOG_RECORD_TO_CALL_RECORD = "redis.bind_dialog_record_to_call_record"
 
     # 自定义事件
