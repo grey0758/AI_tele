dfa1c71e689cd5a9f7e8c18bdbbf49e87e7f5265
grey <2437313414@qq.com>
2025-10-15 16:26:33 +0800

    Remove unused event for hello message in realtime service

--- app/services/phone_service.py
+++ app/services/phone_service.py
@@ -164,7 +164,7 @@ class PhoneService(BaseService):
                 asyncio.run(self.emit_event(EventType.PHONE_SERVICE_ONANSWER, on_message))
 
                 self.call_record.call_id = on_message.uuid
-                self.call_record.instance = on_message.instance
+                self.call_record.instance = self.device_info.devices[on_message.instance].instance
 
                 tts_opening = self.call_record.tts_opening if self.call_record else ""
 
